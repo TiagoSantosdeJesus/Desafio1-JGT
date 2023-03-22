4d7319d971fd9b0de0cdb67c38ae3a6b96e56c2a
khadidjamoraes <khadidja.developer@gmail.com>
2023-03-22 12:00:52 -0300

    Update db2.js

--- models/db2.js
+++ models/db2.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('upconsult', 'root', '1234', {
+const sequelize = new Sequelize('upconsult', 'root', 'Senha', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
