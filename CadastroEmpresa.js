457fe756b75b95e3627d0566f8a074d75fae7d5d
khadidjamoraes <khadidja.developer@gmail.com>
2023-03-22 11:04:42 -0300

    Create CadastroEmpresa.js

---
+++ CadastroEmpresa.js
@@ -0,0 +1,36 @@
+const db2 = require('./db2');
+const seq = db2.Sequelize 
+
+const CadastroEmpresa = db.sequelize.define('Cadastro_Empresa', {
+    Nome_responsavel: {
+        type: seq.STRING,
+        allowNull: false
+    },
+    Nome_Empresa: {
+        type: seq.STRING,
+        allowNull: false
+    },    
+    CPF_CNPJ: {
+        type: seq.INT,
+        primaryKey: true,
+        allowNull: false
+    },   
+    Email: {
+        type: seq.STRING,
+        allowNull: false
+    },   
+    Senha: {
+        type: seq.INT,
+        allowNull: false
+    },   
+    DataNasc_Criacao: {
+        type: seq.DATE,
+        allowNull: true
+    },   
+    Endereco: {
+        type: seq.STRING,
+        allowNull: true
+    }   
+})
+    
+module.exports = CadastroEmpresa;
