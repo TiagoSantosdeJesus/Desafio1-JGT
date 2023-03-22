f20e49e48f64b8adbc06007d247ca39279f913da
khadidjamoraes <khadidja.developer@gmail.com>
2023-03-22 10:12:42 -0300

    sequelize

---
+++ db2.js
@@ -0,0 +1,13 @@
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('upconsult', 'root', 'Khadidja', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    define: {
+        timestamps: false
+    } 
+})
+
+module.exports = sequelize; 
