38440187f6a95644149f88a5b88be0bf45c08827
khadidjamoraes <khadidja.developer@gmail.com>
2023-03-22 12:01:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const db = require("./models/db2");
+
+(async () => {
+    const db2 = require("./models/db2");
+    const cadastroEmpresa = require('./models/CadastroEmpresa');
+    const cadastroConsultor = require('./models/cadastroConsultor');
+
+    try {
+        const resultado = await db2.sync();
+        console.log(resultado);
+    } catch (error) {
+        console.log(error);
+    }
+})();
